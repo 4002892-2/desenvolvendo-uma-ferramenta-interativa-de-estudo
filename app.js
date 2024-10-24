0a73902e8583aefef3551786fb619d5d233998d9
Veronica Maria <00001120201482sp@al.educacao.sp.gov.br>
2024-10-24 07:31:01 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+Analise se as afirmações a seguir estão certas ou erradas.
+
+A) O par ordenado ﻿
+(
+D
+;
+H
+)
+=
+(
+5
+;
+120
+)
+(D;H)=(5;120)﻿ representa que 5 dias têm 120 horas.
+
+Certo
+Errado
+B) O gráfico que relaciona a quantidade de dias (D) com a quantidade de horas (H) tem o formato circular.
+
+Certo
+Errado
+C) O gráfico que relaciona a quantidade de dias (D) com a quantidade de horas (H) passa pela origem do plano cartesiano.
+
+Certo
+Errado
+Obrigatória
+
